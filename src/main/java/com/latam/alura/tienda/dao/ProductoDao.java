0ef52db5c0b9bfa251ab8d31eb92befca08e9982
Juan Garcia <Juanmanu2350@gmail.com>
2023-05-13 23:47:36 -0600

    Metodos de Consultas agregados

--- src/main/java/com/latam/alura/tienda/dao/ProductoDao.java
+++ src/main/java/com/latam/alura/tienda/dao/ProductoDao.java
@@ -1,5 +1,6 @@
 package com.latam.alura.tienda.dao;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -26,6 +27,16 @@ public class ProductoDao {
 	public List<Producto> consultarTodos(){
 		String jqpl = "SELECT P FROM Producto AS P";
 		return em.createQuery(jqpl, Producto.class).getResultList();
+	}
+	
+	public List<Producto> consultaPorNombreDeCategoria(String nombre){
+		String jpql="SELECT p FROM Producto AS p WHERE p.categoria.nombre=:nombre";
+		return em.createQuery(jpql,Producto.class).setParameter("nombre", nombre).getResultList();
+	}
+	 
+	public BigDecimal consultarPrecioPorNombreDeProducto(String nombre) {
+		String jpql="SELECT P.precio FROM Producto AS P WHERE P.nombre=:nombre";
+		return em.createQuery(jpql,BigDecimal.class).setParameter("nombre", nombre).getSingleResult();
 	}
 
 }
--- src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
+++ src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
@@ -18,35 +18,33 @@ public class RegistroDeProducto {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		registrarProducto();
+		registrarProducto();
 		EntityManager em = JPAUtils.getEntityManager();
-		
-		ProductoDao productoDao = new ProductoDao(em);
-		
-		Producto producto = productoDao.consultaPorId(1l);
-		System.out.println(producto.getNombre());
-		
-		List<Producto> productos = productoDao.consultarTodos();
-		productos.forEach(prod -> System.out.println(prod));
+	    ProductoDao productoDao = new ProductoDao(em);
+	    Producto producto = productoDao.consultaPorId(1l);
+	    System.out.println(producto.getNombre());
+	    
+	    BigDecimal precio = productoDao.consultarPrecioPorNombreDeProducto("Xiaomi Redmi");
+	    System.out.println(precio);
 		
 	}
 
 	private static void registrarProducto() {
-		Categoria celulares = new Categoria("Celulares");
-		Producto celular = new Producto("Samsung","telefono usado", new BigDecimal("1000"), celulares);
-		
-		
-		EntityManager em = JPAUtils.getEntityManager();
-		
-		ProductoDao productoDao = new ProductoDao(em);
-		CategoriaDao categoriaDao = new CategoriaDao(em);
-		
-		em.getTransaction().begin();
-		
-		categoriaDao.guardar(celulares);
-		productoDao.guardar(celular);
-		
-		em.getTransaction().commit();
-		em.clear();
+		Categoria celulares = new Categoria("CELULARES");
+
+		Producto celular = new Producto("Xiaomi Redmi", "Muy bueno", new BigDecimal("800"), celulares);
+
+	    EntityManager em = JPAUtils.getEntityManager();
+	    ProductoDao productoDao = new ProductoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+        
+	    em.getTransaction().begin();
+	    
+	    categoriaDao.guardar(celulares);
+	    productoDao.guardar(celular);	
+	    
+	    em.getTransaction().commit();
+	    em.close();
 	}
 
 }
